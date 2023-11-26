93d40229233ef510b52470f97423d6cbb591db6c
Safdar Sikander <safdar_sikander@live.com>
2023-11-26 18:04:59 +0500

    Exclude Password property method 1

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,8 @@ import {
   Delete,
   Patch,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -21,6 +23,7 @@ export class UsersController {
     return await this.usersService.create(body.email, body.password);
   }
   @Get('/:id')
+  @UseInterceptors(ClassSerializerInterceptor)
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
