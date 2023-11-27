7b70ab2a9cc76d1c2543eb4b4cea894e67887fa6
Safdar Sikander <safdar_sikander@live.com>
2023-11-27 08:38:23 +0500

    Moved UserDto controller level

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Post('/signup')
@@ -24,7 +25,6 @@ export class UsersController {
   }
 
   @Get('/:id')
-  @Serialize(UserDto)
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
