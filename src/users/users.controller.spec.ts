59f4424376a8b5f562060b815a3e570c5ac5b22b
Safdar Sikander <safdar_sikander@live.com>
2023-12-03 17:37:39 +0500

    add signin controller test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -11,8 +11,10 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     fakeAuthService = {
-      // signup: (email: string, password: string) => {},
-      // signin: () => {},
+      signup: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+      signin: (email, password) =>
+        Promise.resolve({ id: 1, email, password } as User),
     };
     fakeUsersService = {
       find: (email: string) =>
@@ -41,5 +43,30 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all users from the database', async () => {
+    const users = await controller.findAllUsers('a@a.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('a@a.com');
+  });
+
+  it('findUser return a single user with the provided id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow();
+  });
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'a@a.com', password: '123' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
