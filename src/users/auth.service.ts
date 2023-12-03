cea678945c61db7e4cc3ec978e0635700751029f
Safdar Sikander <safdar_sikander@live.com>
2023-12-03 12:43:34 +0500

    Add signin in auth service

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -9,9 +13,21 @@ const scrypt = promisify(_scrypt);
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  signin(email: string, password: string) {}
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Wrong password');
+    }
+    return user;
+  }
 
   async signup(email: string, password: string) {
+    console.log('----------------------signup')
     const user = await this.usersService.find(email);
     if (user.length) {
       throw new BadRequestException('User already exists');
