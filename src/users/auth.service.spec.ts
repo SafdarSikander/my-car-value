4d205accf828703e5c9834adb6ee9eee74ff936a
Safdar Sikander <safdar_sikander@live.com>
2023-12-03 15:05:07 +0500

    Add auth instance test

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,29 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+it(`can create a instance of AuthService`, async () => {
+  // create fake service of UserService
+  const fakeUsersService: Partial<UsersService> = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({
+        id: 1,
+        email,
+        password,
+      } as User),
+  };
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
+  }).compile();
+
+  const service = module.get(AuthService);
+  expect(service).toBeDefined();
+});
